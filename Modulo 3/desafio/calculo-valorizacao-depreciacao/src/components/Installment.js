0ba238ceac55be427956722a57ca4c22a413e33c
Dougllas Diniz <dmd.dougllas@gmail.com>
2020-06-21 12:01:47 -0300

    melhorias

--- Modulo 3/desafio/calculo-valorizacao-depreciacao/src/components/Installment.js
+++ Modulo 3/desafio/calculo-valorizacao-depreciacao/src/components/Installment.js
@@ -2,24 +2,15 @@ import React, { useState, useEffect } from 'react';
 import { CalcAmount } from '../helpers/CalcAmount';
 
 export default function Installment({ finalValue, difference, percent }) {
-  const [qdtInstallment, setQtdInstallment] = useState([]);
+  const [qtdInstallment, setQtdInstallment] = useState([]);
   let elementHTML = '';
 
   useEffect(() => {
-    for (let index = 1; index <= percent; index++) {
-      const { getAmount, getDifference, getRoot } = CalcAmount(
-        finalValue,
-        difference,
-        percent
-      );
-      setQtdInstallment({
-        id: index,
-        amount: getAmount,
-        difference: getDifference,
-        root: getRoot,
-      });
-    }
+    setQtdInstallment(CalcAmount(finalValue, difference, percent));
   }, [finalValue, difference, percent]);
-  console.log(qdtInstallment);
-  return <div> </div>;
+  qtdInstallment.map((element) => {
+    elementHTML += `${element}`;
+    return console.log(element);
+  });
+  return <div>{elementHTML}</div>;
 }
--- Modulo 3/desafio/calculo-valorizacao-depreciacao/src/helpers/CalcAmount.js
+++ Modulo 3/desafio/calculo-valorizacao-depreciacao/src/helpers/CalcAmount.js
@@ -1,15 +1,20 @@
 function CalcAmount(value, rate, period) {
-  const amount = (value * Math.pow(1 + rate / 100, period)).toFixed(2);
-  const difference = (amount - value).toFixed(2);
-  let root = ((difference * 100) / value).toFixed(2);
-  if (isNaN(root)) {
-    root = '0.00';
+  let arrayAmount = [];
+  for (let index = 1; index <= period; index++) {
+    const amount = (value * Math.pow(1 + rate / 100, index)).toFixed(2);
+    const difference = (amount - value).toFixed(2);
+    let root = ((difference * 100) / value).toFixed(2);
+    if (isNaN(root)) {
+      root = '0.00';
+    }
+    arrayAmount.push({
+      id: index,
+      getAmount: amount,
+      getDifference: difference,
+      getRoot: root,
+    });
   }
-  return {
-    getAmount: amount,
-    getDifference: difference,
-    getRoot: root,
-  };
+  return arrayAmount;
 }
 
 export { CalcAmount };
